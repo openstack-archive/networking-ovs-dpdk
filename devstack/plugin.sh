ec865098609780350ab233dd280ecd0e253abf05
Michal Ptacek <michalx.ptacek@intel.com>
2015-09-10 15:58:10 +0100

    Add support for installation mode option
    Introducing new options:
    OVS_DPDK_MODE and OVS_DPDK_INSTALL
    
    Closes-bug: #1490983
    Change-Id: I34f8b0f5e7ec0afce80eb44239a853a44d023a4f

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -10,18 +10,30 @@ set -x
             case $2 in
                 "pre-install")
                     # cloning source code
-                    echo_summary "Downloading Dependencies for OVS DPDK"
-                    clone_ovs_dpdk
+                    if [ $OVS_DPDK_INSTALL == 'True' ]; then
+                        echo_summary "Downloading Dependencies for OVS DPDK"
+                        clone_ovs_dpdk
+                    else
+                        echo_summary "Cloning of src files for ovs-dpdk not required"
+                    fi
                 ;;
                 "install")
                     # Perform installation of ovs dpdk
-                    echo_summary "Configuring, installing and starting OVS DPDK"
-                    update_ovs_pmd_core_mask
-                    install_ovs_dpdk
-                    pushd $NETWOKING_OVS_DPDK_DIR
-                    sudo python setup.py install
-                    popd
-                    start_ovs_dpdk
+                    if [ $OVS_DPDK_INSTALL == 'True' ]; then
+                        echo_summary "Configuring, installing and starting OVS DPDK"
+                        update_ovs_pmd_core_mask
+                        install_ovs_dpdk
+                        pushd $NETWOKING_OVS_DPDK_DIR
+                        sudo python setup.py install
+                        popd
+                        start_ovs_dpdk
+                    else
+                        echo_summary "OVS_DPDK_INSTALL configured for not to install OVS DPDK"
+                        echo_summary "Agent & ovsdpdk mechanism driver has to be built anyway"
+                        pushd $NETWOKING_OVS_DPDK_DIR
+                        sudo python setup.py install
+                        popd
+                    fi
                 ;;
                 "post-config")
                     set_vcpu_pin_set
