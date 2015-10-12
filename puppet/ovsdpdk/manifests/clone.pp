1243ef861d89d0a3b50e33c68d57386b6d01cb79
Michal Ptacek <michalx.ptacek@intel.com>
2015-10-12 13:18:04 +0100

    Adding ovsdpdk puppet module
    This module allows people to install OVS with DPDK support
    via puppet manifests. So it should be used as another
    supported deployment method in parallel with devstack.
    
    Change-Id: Ic3fa57b29170b67834f62556cbe10f3b4b382d49

---
+++ puppet/ovsdpdk/manifests/clone.pp
@@ -0,0 +1,47 @@
+# == Class: ovsdpdk::clone
+#
+# Responsible for downloading all relevant git repos for setting up of OVS+DPDK
+#
+class ovsdpdk::clone(
+  $dest                    = $::ovsdpdk::params::dest,
+  $ovs_dir                 = $::ovsdpdk::params::ovs_dir,
+  $ovs_dpdk_dir            = $::ovsdpdk::params::ovs_dpdk_dir,
+  $networking_ovs_dpdk_dir = $::ovsdpdk::params::networking_ovs_dpdk_dir,
+  $ovs_git_tag             = $::ovsdpdk::params::ovs_git_tag,
+  $ovs_dpdk_git_tag        = $::ovsdpdk::params::ovs_dpdk_git_tag,
+  $ovs_plugin_git_tag      = $::ovsdpdk::params::ovs_plugin_git_tag,
+) inherits ovsdpdk {
+
+  file { $dest:
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  package { 'git':
+    ensure   => installed,
+  }
+
+  vcsrepo { $ovs_dir:
+    ensure   => present,
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'https://github.com/openvswitch/ovs.git',
+    revision => $ovs_git_tag,
+  }
+
+  vcsrepo { $ovs_dpdk_dir:
+    ensure   => present,
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'http://dpdk.org/git/dpdk',
+    revision => $ovs_dpdk_git_tag,
+  }
+
+  vcsrepo { $networking_ovs_dpdk_dir:
+    ensure   => present,
+    provider => git,
+    require  => [ Package['git'] ],
+    source   => 'https://github.com/stackforge/networking-ovs-dpdk',
+    revision => $ovs_plugin_git_tag,
+  }
+}
---
+++ puppet/ovsdpdk/manifests/params.pp
@@ -0,0 +1,32 @@
+#
+# This class contains the platform differences for ovsdpdk
+# and environment not commonly configured stuff
+#
+class ovsdpdk::params {
+
+  $ovs_db_conf_dir          = '/etc/openvswitch'
+  $ovs_db_socket_dir        = '/var/run/openvswitch'
+  $ovs_db_socket            = "${ovs_db_socket_dir}/db.sock"
+  $ovs_db_conf              = "${ovs_db_conf_dir}/conf.db"
+
+  # General config
+  $plugin_dir               = '/etc/puppet/modules/ovsdpdk'
+  $dest                     = '/opt/code'
+  $nova_conf_dir            = '/etc/nova'
+  $nova_conf                = "${nova_conf_dir}/nova.conf"
+
+  # OVS config
+  $ovs_git_repo             = 'https://github.com/openvswitch/ovs.git'
+  $ovs_dir                  = "${dest}/ovs"
+  $ovs_git_tag              = '1e77bbe565bbf5ae7f4c47f481a4097d666d3d68'
+
+  # DPDK config
+  $ovs_dpdk_git_repo        = 'http://dpdk.org/git/dpdk'
+  $ovs_dpdk_git_tag         = 'v2.0.0'
+  $ovs_dpdk_dir             = "${dest}/DPDK-${ovs_dpdk_git_tag}"
+
+  # PLUGIN config
+  $networking_ovs_dpdk_dir  = "${dest}/networking-ovs-dpdk"
+  $ovs_plugin_git_tag       = 'master'
+}
+
