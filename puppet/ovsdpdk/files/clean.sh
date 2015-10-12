f6403df0f139c5ae80dde9ccb6f332112996ee3c
Michal Ptacek <michalx.ptacek@intel.com>
2015-10-12 13:18:04 +0100

    Adding ovsdpdk puppet module
    This module allows people to install OVS with DPDK support
    via puppet manifests. So it should be used as another
    supported deployment method in parallel with devstack.
    
    Change-Id: Ic3fa57b29170b67834f62556cbe10f3b4b382d49

---
+++ puppet/ovsdpdk/files/clean.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env	bash
+
+# Short script for uninstallation of OVS with DPDK
+
+# enable debugging:
+#set -o xtrace
+
+# Stopping service if needed
+echo "[debug] stopping ovs-dpdk (if needed)"
+sudo service ovs-dpdk status 2>&1 1>/dev/null
+res=$?
+
+if [ $res -eq 2 ]; then
+    echo "[warning] service pvs-dpdk is already stopped"
+else
+    echo "[debug] stopping ovs-dpdk service"
+    sudo service ovs-dpdk stop 2>&1 1>/dev/null
+fi
+
+# Remove  ovs-dpdk service
+echo "[debug] removing ovs-dpdk service"
+sudo rm -f /etc/default/ovs-dpdk
+sudo rm -f /etc/init.d/ovs-dpdk
+
+# Remove mech driver
+echo "[debug] removing mech driver"
+sudo rm -f /usr/bin/networking-ovs-dpdk-agent
+
+# kvm wrapper
+echo "[debug] replacing kvm wrapper"
+if [ -e /usr/bin/kvm ]; then
+    KVM_CMD="/usr/bin/kvm"
+elif [ -e /usr/bin/qemu-kvm ]; then
+    KVM_CMD="/usr/bin/qemu-kvm"
+else
+    echo "[warning] package qemu-kvm probably not installed"
+fi
+
+cat << 'EOF' | sudo tee  $KVM_CMD
+#!/bin/sh
+
+exec /usr/bin/qemu-system-x86_64  "${args[@]}"
+EOF
+
+
+# remove git repos
+echo "[warning] for security reasons, git repos were not automatically removed, you can remove it once you're finished with them"
