c364a1cbf5a4bf7adadcebb543e4ff25bfc67757
Michal Ptacek <michalx.ptacek@intel.com>
2015-12-14 16:44:38 +0000

    Solving integration issues with liberty puppet openstack
    Following topics are addressed in this commit:
    - due to package dependencies in puppet environment
      vanilla openvswitch package can't be simply removed
      but built binaries are replacing original files instead
    - dependent neutron services refreshed after ovs-dpdk is deployed
    - standard openstack bridges deployed on empty ovs db
    
    Change-Id: Ice6b22568d007bdcc89adfa8b63f0f537af0f3be
    Closes-Bug: #1523810

--- puppet/ovsdpdk/manifests/uninstall_ovs.pp
+++ puppet/ovsdpdk/manifests/uninstall_ovs.pp
@@ -2,29 +2,35 @@
 #
 # Provides uninstallation of openvswitch package (if present) together with removing of kernel module
 #
-class ovsdpdk::uninstall_ovs () inherits ovsdpdk {
+class ovsdpdk::uninstall_ovs (
+  $openvswitch_service_name = $::ovsdpdk::params::openvswitch_service_name,
+  $openvswitch_agent        = $::ovsdpdk::params::openvswitch_agent,
+  $install_packages         = $::ovsdpdk::params::install_packages,
+  $openvswitch_agent        = $::ovsdpdk::params::openvswitch_agent,
+) inherits ovsdpdk {
 
-  case $::operatingsystem {
-    'Ubuntu': {
-      $remove_packages = [ 'openvswitch-switch', 'openvswitch-datapath-dkms', 'openvswitch-common' ]
-      $install_packages = [ 'autoconf', 'libtool', 'libfuse-dev', 'screen' ]
-    }
-    default: {
-      $remove_packages = [ 'openvswitch' ]
-      $install_packages = [ 'pciutils', 'autoconf', 'libtool', 'fuse-devel', 'screen' ]
+  #Due to dependencies to other packages, we won't purge vanilla OVS  
+  #package { $remove_packages: ensure => 'purged' }
 
-      package { "kernel-devel-${::kernelversion}":
-        ensure          => 'installed',
-        provider        => 'yum',
-        install_options => '--showduplicates',
-      }
-    }
+  exec { "/usr/sbin/service ${openvswitch_service_name} stop":
+    user => root,
   }
 
-  package { $remove_packages: ensure => 'purged' }
+  exec { "/usr/sbin/service ${openvswitch_agent} stop":
+    user => root,
+  }
+
+  exec { '/usr/sbin/service neutron-server stop':
+    user => root,
+  }
 
   package { $install_packages: ensure => 'installed' }
 
-  exec { '/sbin/modprobe -r openvswitch': onlyif => "/bin/grep -q '^openvswitch' '/proc/modules'" }
+  exec { '/sbin/modprobe -r openvswitch':
+    onlyif  => "/bin/grep -q '^openvswitch' '/proc/modules'",
+    user    => root,
+    require => Exec["/usr/sbin/service ${openvswitch_agent} stop"],
+  }
+
 }
 
