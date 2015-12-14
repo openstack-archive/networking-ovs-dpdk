3c630f78558ebefb3c83d47987fef99ebefb4122
Michal Ptacek <michalx.ptacek@intel.com>
2015-12-14 16:44:38 +0000

    Solving integration issues with liberty puppet openstack
    Following topics are addressed in this commit:
    - due to package dependencies in puppet environment
      vanilla openvswitch package can't be simply removed
      but built binaries are replacing original files instead
    - dependent neutron services reconfigured after ovs-dpdk is deployed
    - existing openstack bridges are moved to new datapath
    
    Change-Id: Ice6b22568d007bdcc89adfa8b63f0f537af0f3be
    Closes-Bug: #1523810

--- puppet/ovsdpdk/examples/install_small.pp
+++ puppet/ovsdpdk/examples/install_small.pp
@@ -1,16 +1,22 @@
-# Declaring OVS-DPDK manifests with some low-level mem requirements
-# One can add also other params but OVS_BRIDGE_MAPPINGS is mandatory,
-# please fill it in with network:bridge pairs
-# e.g.: OVS_BRIDGE_MAPPINGS=default:br-enp9s0f0
+# Declaring OVS-DPDK manifests with minimal config changes
+
+# Please don't forget to insert following mandatory params before execution:
+# ovs_bridge_mappings - script is taking nic's for dpdk driver from it
+#                       please fill it in with network:bridge pairs
+# e.g. ovs_bridge_mappings => 'default:br-eth2',
+#
+# openrc_file         - path to openc file required for openstack cli commands
+# e.g. openrc_file         => '/root/openrc'
+
 # You can launch it with enabled verbosity as follows:
-# puppet apply install_small.pp --certname ovsdpdk-install -d -v
+# sudo puppet apply install_small.pp --certname ovsdpdk-install -d -v
 
 node /ovsdpdk-install/ {
 
   class { '::ovsdpdk':
-    ovs_bridge_mappings => '',
-    ovs_socket_mem      => '512',
-    ovs_num_hugepages   => '300',
+    ovs_bridge_mappings => 'default:br-eth2',
+    openrc_file         => '/root/openrc',
+    ovs_socket_mem      => '2048',
+    ovs_num_hugepages   => '2048',
   }
-
 }
--- puppet/ovsdpdk/manifests/install_ovs_dpdk.pp
+++ puppet/ovsdpdk/manifests/install_ovs_dpdk.pp
@@ -5,14 +5,21 @@
 # options and setting huge pages into shared mode
 #
 class ovsdpdk::install_ovs_dpdk (
-  $networking_ovs_dpdk_dir          = $::ovsdpdk::networking_ovs_dpdk_dir,
+  $networking_ovs_dpdk_dir          = $::ovsdpdk::params::networking_ovs_dpdk_dir,
   $plugin_dir                       = $::ovsdpdk::params::plugin_dir,
+  $ovs_dir                          = $::ovsdpdk::params::ovs_dir,
+  $openvswitch_service_file         = $::ovsdpdk::params::openvswitch_service_file,
+  $openvswitch_service_path         = $::ovsdpdk::params::openvswitch_service_path,
+  $qemu_kvm                         = $::ovsdpdk::params::qemu_kvm,
 ) inherits ovsdpdk {
   require ovsdpdk::build_ovs_dpdk
 
   exec {'create_ovs_dpdk':
-    command => "cp ${networking_ovs_dpdk_dir}/devstack/ovs-dpdk/ovs-dpdk-init /etc/init.d/ovs-dpdk;chmod +x /etc/init.d/ovs-dpdk",
-    creates => '/etc/init.d/ovs-dpdk',
+    command => "mv /etc/init.d/openvswitch-switch /tmp/openvswitch-switch.bak;\
+                cp ${networking_ovs_dpdk_dir}/devstack/ovs-dpdk/ovs-dpdk-init /etc/init.d/openvswitch-switch;\
+                chmod +x /etc/init.d/openvswitch-switch;\
+                ln -sf /etc/init.d/openvswitch-switch /etc/init.d/ovs-dpdk;\
+                cp /etc/openvswitch/conf.db /etc/openvswitch/conf.db.pre_dpdk",
     user    => root,
     path    => ['/usr/bin','/bin'],
   }
@@ -20,46 +27,72 @@ class ovsdpdk::install_ovs_dpdk (
   file {'/etc/default/ovs-dpdk': content => template("${plugin_dir}/files/ovs-dpdk-conf.erb"), mode => '0644' }
 
   exec {'adapt_conf_file':
-    command => "${plugin_dir}/files/tune_params.sh",
+    command   => "${plugin_dir}/files/tune_params.sh",
+    user      => root,
+    require   => File['/etc/default/ovs-dpdk'],
+    logoutput => true,
+  }
+
+  exec { 'update ovs service':
+    command => "cp ${plugin_dir}/files/${openvswitch_service_file} ${openvswitch_service_path}/${openvswitch_service_file}",
+    path    => ['/usr/bin','/bin'],
     user    => root,
-    require => File['/etc/default/ovs-dpdk'],
+    onlyif  => "test -f ${openvswitch_service_path}/${openvswitch_service_file}",
+  }
+
+  if $::operatingsystem == 'CentOS' {
+    exec { 'systemctl daemon-reload':
+      path    => ['/usr/bin','/bin','/usr/sbin'],
+      user    => root,
+      require => Exec['update ovs service'],
+    }
   }
 
   package {'qemu-kvm': ensure => installed }
 
-  exec {'replace kvm binary':
-    command => "cp ${plugin_dir}/files/kvm-wrapper.sh /usr/bin/kvm;chmod +x /usr/bin/kvm",
+  exec { "cp ${qemu_kvm} ${qemu_kvm}.orig":
     path    => ['/usr/bin','/bin'],
     user    => root,
-    onlyif  => 'test -f /usr/bin/kvm',
+    onlyif  => "test -f ${qemu_kvm}",
     require => Package['qemu-kvm'],
   }
 
-  exec {'replace qemu-kvm binary':
-    command => "cp ${plugin_dir}/files/kvm-wrapper.sh /usr/bin/qemu-kvm;chmod +x /usr/bin/qemu-kvm",
+  exec { "cp ${plugin_dir}/files/kvm_wrapper ${qemu_kvm};chmod +x ${qemu_kvm}":
     path    => ['/usr/bin','/bin'],
     user    => root,
-    onlyif  => 'test -f /usr/bin/qemu-kvm',
-    require => Package['qemu-kvm'],
+    onlyif  => "test -f ${qemu_kvm}",
+    require => [ Exec["cp ${qemu_kvm} ${qemu_kvm}.orig"], Package['qemu-kvm'] ],
   }
 
-  exec {'init ovs-dpdk':
-    command => '/etc/init.d/ovs-dpdk init',
-    user    => root,
-    require => [ Exec['create_ovs_dpdk'], File['/etc/default/ovs-dpdk'] ],
+  ## This was required on some Ubuntu installations - from fuel
+  #exec { 'ln -sf /etc/init.d/libvirtd /etc/init.d/libvirt-bin':
+  #  path    => ['/usr/bin','/bin'],
+  #  user    => root,
+  #  onlyif  => 'test -f /etc/init.d/libvirtd',
+  #}
+
+  # schema convert required as we are not removing original db
+  exec { "ovsdb-tool convert /etc/openvswitch/conf.db ${ovs_dir}/vswitchd/vswitch.ovsschema":
+    path      => ['/usr/bin','/bin'],
+    user      => root,
+    logoutput => true,
   }
 
-  service {'ovs-dpdk':
-    ensure    => 'running',
-    hasstatus => true,
-    require   => Exec['init ovs-dpdk'],
+  # patching of linux_net.py is required for removing error when setting of MTU
+  exec { 'patch linux_net':
+    command   => "${plugin_dir}/files/linux_net.sh",
+    user      => root,
+    path      => ['/usr/bin','/bin'],
+    logoutput => true,
+    onlyif    => 'test -f /usr/lib/python2.7/dist-packages/nova/network/linux_net.py',
   }
 
   # install mech driver
-  exec {'install mech driver':
-    command => 'python setup.py install',
-    path    => ['/usr/bin','/bin'],
-    cwd     => "${networking_ovs_dpdk_dir}",
-    user    => root,
-  }
+  #exec {'install mech driver':
+  #  command   => 'python setup.py install',
+  #  path      => ['/usr/bin','/bin'],
+  #  cwd       => "${networking_ovs_dpdk_dir}",
+  #  user      => root,
+  #  logoutput => true,
+  #}
 }
