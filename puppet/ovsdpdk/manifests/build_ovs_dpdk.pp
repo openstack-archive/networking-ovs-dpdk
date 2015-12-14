2d9f915d75b8066f7bdb067d5ec6ef52d74e583d
Michal Ptacek <michalx.ptacek@intel.com>
2015-12-14 16:44:38 +0000

    Solving integration issues with liberty puppet openstack
    Following topics are addressed in this commit:
    - due to package dependencies in puppet environment
      vanilla openvswitch package can't be simply removed
      but built binaries are replacing original files instead
    - dependent neutron services refreshed after ovs-dpdk is deployed
    - standard openstack bridges deployed on empty ovs db
    
    Change-Id: Ice6b22568d007bdcc89adfa8b63f0f537af0f3be
    Closes-Bug: #1523810

--- puppet/ovsdpdk/manifests/build_ovs_dpdk.pp
+++ puppet/ovsdpdk/manifests/build_ovs_dpdk.pp
@@ -7,13 +7,13 @@ class ovsdpdk::build_ovs_dpdk (
 ) inherits ovsdpdk {
   require ovsdpdk::uninstall_ovs
 
-  file {"$plugin_dir/files/build_ovs_dpdk.sh":
-      content => template("$plugin_dir/files/build_ovs_dpdk.erb"),
+  file {"${plugin_dir}/files/build_ovs_dpdk.sh":
+      content => template("${plugin_dir}/files/build_ovs_dpdk.erb"),
       mode    => '0775',
   }
 
-  exec {"$plugin_dir/files/build_ovs_dpdk.sh":
-      require => File["$plugin_dir/files/build_ovs_dpdk.sh"],
+  exec {"${plugin_dir}/files/build_ovs_dpdk.sh":
+      require => File["${plugin_dir}/files/build_ovs_dpdk.sh"],
       timeout => 0,
   }
 }
