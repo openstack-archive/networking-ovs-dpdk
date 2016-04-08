8f51733049512606b963cbc0eb6290395d233196
Michal Ptacek <michalx.ptacek@intel.com>
2016-04-08 08:03:53 +0100

    Adding multinode scenario into ovsdpdk puppet module
    Following changes are addresses in this commit:
    - adding separated compute & controller scenario
    - moving configuration changes to standard puppet openstack providers
    - documentation changes
    - some mitaka related tuning
    
    Change-Id: Id516c46cefc2f5875894cd69ea200ce524d8ed63

--- puppet/ovsdpdk/manifests/params.pp
+++ puppet/ovsdpdk/manifests/params.pp
@@ -7,19 +7,17 @@ class ovsdpdk::params {
   case $::operatingsystem {
     'Ubuntu': {
       $qemu_kvm = '/usr/bin/kvm'
-      # we are not removing packages as of now
-      #$remove_packages = [ 'openvswitch-switch', 'openvswitch-datapath-dkms', 'openvswitch-common' ]
-      $install_packages = [ 'autoconf', 'libtool', 'screen', 'bc' ]
+      $install_packages = [ 'git', 'screen', 'patch', 'autoconf', 'libtool', 'bc',
+                            'python-dev', 'python-pip', 'qemu-kvm' ]
       $openvswitch_service_name = 'openvswitch-switch'
       $openvswitch_service_file = 'openvswitch-switch.conf'
       $openvswitch_service_path = '/etc/init'
-      $openvswitch_agent = 'neutron-plugin-openvswitch-agent'
+      $openvswitch_agent = 'neutron-openvswitch-agent'
     }
     'CentOS': {
       $qemu_kvm = '/usr/libexec/qemu-kvm'
-      # we are not removing packages as of now
-      #$remove_packages = [ 'openvswitch' ]
-      $install_packages = [ 'pciutils', 'autoconf', 'libtool', 'screen', 'bc' ]
+      $install_packages = [ 'git', 'screen', 'patch', 'pciutils', 'autoconf', 'libtool', 'bc',
+                            'python-dev', 'python-pip', 'qemu-kvm' ]
       $openvswitch_service_name = 'openvswitch'
       $openvswitch_service_file = 'openvswitch.service'
       $openvswitch_service_path = '/usr/lib/systemd/system'
@@ -40,9 +38,6 @@ class ovsdpdk::params {
   $dest                     = '/opt/code'
   $nova_conf_dir            = '/etc/nova'
   $nova_conf                = "${nova_conf_dir}/nova.conf"
-  $nova_compute_conf        = "${nova_conf_dir}/nova-compute.conf"
-  $ml2_conf                 = '/etc/neutron/plugins/ml2/ml2_conf.ini'
-  $neutron_l3_conf          = '/etc/neutron/l3_agent.ini'
 
   # OVS config
   $ovs_install_dir          = '/usr'
