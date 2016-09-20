9ddc27efaa5a26fd4232ffbd5f3e72b8d0b0b80b
Waldemar Znoinski <waldemar.znoinski@intel.com>
2016-09-20 13:41:23 +0000

    import securitygroups_rpc from neutron.conf.agent instead of neutron.agent
    Change-Id: I728f628d856c005d389a75a3b9ef55900c575bd8

--- networking_ovs_dpdk/tests/unit/agent/test_ovs_dpdk_firewall.py
+++ networking_ovs_dpdk/tests/unit/agent/test_ovs_dpdk_firewall.py
@@ -22,7 +22,7 @@ from networking_ovs_dpdk.common._i18n import _
 from neutron.agent.common import config as a_cfg
 from neutron.agent.common import ovs_lib
 from neutron.agent.ovsdb import api as ovsdb
-from neutron.agent import securitygroups_rpc as sg_cfg
+from neutron.conf.agent import securitygroups_rpc as sg_cfg
 from neutron.plugins.ml2.drivers.openvswitch.agent.openflow.ovs_ofctl \
     import br_int
 from neutron.plugins.ml2.drivers.openvswitch.agent.ovs_agent_extension_api\
